a7607930762d421a9cfba69256f75c10182ac9d0
Aleksandr Andrienko <aandrienko@codenvy.com>
2015-08-28 16:03:37 +0300

    IDEX-2784: Fixed problem with file attributes for Windows. Fixed problem when project workspace contains spaces.

--- che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/ssh/GitSshScript.java
+++ che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/ssh/GitSshScript.java
@@ -20,6 +20,28 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.attribute.AclEntry;
+import java.nio.file.attribute.AclEntryType;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.UserPrincipal;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.nio.file.attribute.AclEntryPermission.APPEND_DATA;
+import static java.nio.file.attribute.AclEntryPermission.DELETE;
+import static java.nio.file.attribute.AclEntryPermission.READ_ACL;
+import static java.nio.file.attribute.AclEntryPermission.READ_ATTRIBUTES;
+import static java.nio.file.attribute.AclEntryPermission.READ_DATA;
+import static java.nio.file.attribute.AclEntryPermission.READ_NAMED_ATTRS;
+import static java.nio.file.attribute.AclEntryPermission.SYNCHRONIZE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_READ;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_WRITE;
+import static org.eclipse.che.api.core.util.SystemInfo.isUnix;
+import static org.eclipse.che.api.core.util.SystemInfo.isWindows;
 
 /**
  * Implementation of script that provide ssh connection
@@ -29,9 +51,10 @@ import java.io.IOException;
 public class GitSshScript {
 
     private static final Logger LOG                 = LoggerFactory.getLogger(GitSshScriptProvider.class);
-    private static final String SSH_SCRIPT_TEMPLATE = "exec ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $ssh_key $@";
+    private static final String SSH_SCRIPT_TEMPLATE = "exec ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $ssh_key $@";//todo check maybe call but ...
     private static final String SSH_SCRIPT          = "ssh_script";
     private static final String DEFAULT_KEY_NAME    = "identity";
+    private static final String OWNER_NAME_PROPERTY = "user.name";
 
     private byte[] sshKey;
     private String host;
@@ -39,7 +62,13 @@ public class GitSshScript {
     private File   sshScriptFile;
 
     public GitSshScript(String host, byte[] sshKey) throws GitException {
-        this.rootFolder = Files.createTempDir();
+        File file = Files.createTempDir();
+        this.rootFolder = new File(file.getAbsolutePath() + File.separator + "test     test");//todo it's for test space in the path
+        try {
+            java.nio.file.Files.createDirectory(rootFolder.toPath());
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+        }
         this.host = host;
         this.sshKey = sshKey;
         this.sshScriptFile = storeSshScript(writePrivateKeyFile().getPath());
@@ -66,16 +95,42 @@ public class GitSshScript {
             throw new GitException("Cant store ssh key. ");
         }
 
-        boolean perms;
-        //set perm to -r--r--r--
-        perms = keyFile.setReadOnly();
-        //set perm to ----------
-        perms &= keyFile.setReadable(false, false);
-        //set perm to -r--------
-        perms &= keyFile.setReadable(true, true);
-        //set perm to -rw-------
-        perms &= keyFile.setWritable(true, true);
-        if (!perms) throw new GitException("Failed to set file permissions");
+        try {
+            if (isUnix()) {
+                //set perm to -rw-------
+                Set<PosixFilePermission> permissions = EnumSet.of(OWNER_READ, OWNER_WRITE);
+                java.nio.file.Files.setPosixFilePermissions(keyFile.toPath(), permissions);
+            } else if (isWindows()) {
+                AclFileAttributeView attributes = java.nio.file.Files.getFileAttributeView(keyFile.toPath(), AclFileAttributeView.class);
+
+                AclEntry.Builder builder = AclEntry.newBuilder();
+                builder.setType(AclEntryType.ALLOW);
+
+                String ownerName = System.getProperty(OWNER_NAME_PROPERTY);
+                UserPrincipal userPrincipal = FileSystems.getDefault().getUserPrincipalLookupService().lookupPrincipalByName(ownerName);
+
+                builder.setPrincipal(userPrincipal);
+                builder.setPermissions(READ_DATA,
+                                       //WRITE_DATA,
+                                       APPEND_DATA,
+                                       READ_NAMED_ATTRS,
+                                       //WRITE_NAMED_ATTRS,
+                                       READ_ATTRIBUTES,
+                                       //WRITE_ATTRIBUTES,
+                                       DELETE,
+                                       READ_ACL,
+                                       //WRITE_ACL,
+                                       SYNCHRONIZE);
+
+                AclEntry entry = builder.build();
+                List<AclEntry> aclEntryList = new ArrayList<>();
+                aclEntryList.add(entry);
+                attributes.setAcl(aclEntryList);
+            }
+        } catch (IOException e) {
+            throw new GitException("Failed to set file permissions");
+        }
+
         this.sshScriptFile = keyFile;
         return keyFile;
     }
@@ -92,7 +147,7 @@ public class GitSshScript {
     private File storeSshScript(String keyPath) throws GitException {
         File sshScriptFile = new File(rootFolder, SSH_SCRIPT);
         try (FileOutputStream fos = new FileOutputStream(sshScriptFile)) {
-            fos.write(SSH_SCRIPT_TEMPLATE.replace("$ssh_key", keyPath).getBytes());
+            fos.write(SSH_SCRIPT_TEMPLATE.replace("$ssh_key", "\"" + keyPath + "\"").getBytes());
         } catch (IOException e) {
             LOG.error("It is not possible to store " + keyPath + " ssh key");
             throw new GitException("Can't store SSH key");
